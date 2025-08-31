7f2712f008c48c5c3154775c1675e6c22b039595
Vitalii Shumakov <fafde8120@gmail.com>
2025-08-31 19:49:35 +0200

    try do artist js

---
+++ src/js/artists.js
@@ -0,0 +1,45 @@
+// Функция рендера карточек артистов
+
+// Находим список, куда будут добавляться карточки артистов
+const artistsEl = document.querySelector(".artist-list");
+
+// Экспортируем функцию для рендера карточек артистов
+export function createArtists(artists) {
+  const markup = artists
+    .map((artist) => {
+      // Если жанров нет — выводим пустой элемент
+      const genreList =
+        !artist.genres || artist.genres.length === 0
+          ? "<li></li>"
+          : artist.genres
+              .map((genre) => `<li class="artist-genre-item">${genre}</li>`)
+              .join("");
+
+      return `
+      <li class="artist-card">
+        <img class="artist-photo" src="${artist.strArtistThumb}" alt="${artist.strArtist} photo">
+        
+        <ul class="artist-genre-list">
+            ${genreList}
+        </ul>
+        
+        <h4 class="artist-name">${artist.strArtist}</h4>
+        
+        <p class="artist-info">${artist.strBiographyEN}</p>
+        
+        <button class="artist-button">
+          Learn More
+          <svg class="artist-button-icon" width="24" height="24">
+              <use href="../img/about-us-img/about-us.svg#icon-icon"></use>
+          </svg>
+        </button>
+      </li>
+      `;
+    })
+    .join("");
+
+  // Добавляем разметку в контейнер
+  artistsEl.insertAdjacentHTML("beforeend", markup);
+
+  console.log("Artists rendered:", artists);
+}
